77f43a9fff5c2a5a05930d701b713942f9d2adf7
+ <+>
2021-10-13 15:49:30 -0400

    Completed asignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 31
+const num2 = 2
 
-// Write your code here
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random() *1) +1
+
+let num3 = 16
+let num4 = 6
+let mod = num3 % num4 
+
+let max = Math.max(1, 2, 14, 7, 20)
